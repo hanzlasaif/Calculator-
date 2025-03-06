f8bd48e78f886f4c22d2d6058220ad7e861647ad
hanzlasaif <hanzlasaif725@gmail.com>
2025-03-07 00:10:25 +0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main() {
     cout << "Your Name - Roll Number";
+     cout << "Subtraction: " << a - b;
+     if (b != 0)
+        cout << "Division: " << a / b;
+    else
+        cout << "Division by zero is not allowed.";
+       cout << "Addition: " << a + b;
+       cout << "Product: " << a * b;
+    cout<<"This change is from conflict_branch1"<<endl;
     return 0;
 }
